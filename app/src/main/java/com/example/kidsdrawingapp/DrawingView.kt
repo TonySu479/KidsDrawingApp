a3c80061a24e728df62634602000418700e91876
TonySu479 <tonysu479@gmail.com>
2021-01-22 17:29:20 +0100

    Update DrawingView.kt

--- app/src/main/java/com/example/kidsdrawingapp/DrawingView.kt
+++ app/src/main/java/com/example/kidsdrawingapp/DrawingView.kt
@@ -1,10 +1,31 @@
 package com.example.kidsdrawingapp
 
 import android.content.Context
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 
 class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
+    private var mDrawPath: CustomPath? = null
+    private var mCanvasBitmap: Bitmap? = null
+    private var mDrawPaint: Paint? = null
+    private var mCanvasPaint: Paint? = null
+    private var mBrushSize: Float = 0.toFloat()
+    private var color = Color.BLACK
+    private var canvas: Canvas? = null
+
+    init{
+        setUpDrawing()
+    }
+
+    private fun setUpDrawing(){
+        mDrawPaint = Paint()
+        
+    }
+
+    internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path() {
+
+    }
 
 }
