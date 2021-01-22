21a74aef95e3c93c9d290656598b0272acca4228
TonySu479 <tonysu479@gmail.com>
2021-01-22 12:36:18 +0100

    First commit

---
+++ app/src/main/java/com/example/kidsdrawingapp/DrawingView.kt
@@ -0,0 +1,10 @@
+package com.example.kidsdrawingapp
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+
+
+}
