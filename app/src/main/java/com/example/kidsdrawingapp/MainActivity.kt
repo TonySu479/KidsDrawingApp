a8e8fa2d5b6df062f9c1cf44b583b0fc511e4656
TonySu479 <tonysu479@gmail.com>
2021-01-24 12:44:35 +0100

    Save Image Function

--- app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -5,6 +5,10 @@ import android.app.Activity
 import android.app.Dialog
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -16,6 +20,9 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.dialog_brush_size.*
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
 
@@ -40,9 +47,20 @@ class MainActivity : AppCompatActivity() {
             if(isReadStorageAllowed()){
 
                 val pickPhotoIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-
                 startActivityForResult(pickPhotoIntent, GALLERY)
 
+            } else {
+                requestStoragePermission()
+            }
+        }
+
+        ib_undo.setOnClickListener{
+            drawing_view.onClickUndo()
+        }
+
+        ib_save.setOnClickListener{
+            if(isReadStorageAllowed()){
+                BitmapAsyncTask(getBitmapFromView(fl_drawing_view_container)).execute()
             } else {
                 requestStoragePermission()
             }
@@ -142,6 +160,67 @@ class MainActivity : AppCompatActivity() {
         val result = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
         return result == PackageManager.PERMISSION_GRANTED
     }
+
+    private fun getBitmapFromView(view: View) : Bitmap {
+
+        val returnedBitmap = Bitmap.createBitmap(view.width,
+            view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(returnedBitmap)
+        val bgDrawable = view.background
+        if(bgDrawable != null){
+            bgDrawable.draw(canvas)
+        }else{
+            canvas.drawColor(Color.WHITE)
+        }
+        view.draw(canvas)
+        return returnedBitmap
+    }
+
+    private inner class BitmapAsyncTask(val mBitmap: Bitmap):
+        AsyncTask<Any, Void, String>(){
+
+        override fun doInBackground(vararg params: Any?): String {
+            var result = ""
+            if(mBitmap != null){
+                try {
+                    val bytes = ByteArrayOutputStream()
+                    mBitmap.compress(Bitmap.CompressFormat.PNG, 90, bytes)
+                    val f = File(externalCacheDir!!.absoluteFile.toString()
+                            + File.separator
+                            + "KidDrawingApp_"
+                            + System.currentTimeMillis() / 1000 + ".png")
+                    val fos = FileOutputStream(f)
+                    fos.write(bytes.toByteArray())
+                    fos.close()
+                    result = f.absolutePath
+                } catch(e: Exception){
+                    result = ""
+                    e.printStackTrace()
+                }
+            }
+            return result
+
+
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            if(!result!!.isEmpty()){
+                Toast.makeText(
+                    this@MainActivity,
+                    "File saved successfully : $result",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+                else{
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Something went wrong while saving file",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
 
     companion object {
         private const val STORAGE_PERMISSION_CODE = 1
